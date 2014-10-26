75beb5593a73e66edefcde9caaeff6e0f7323f73
psm1782 <tjdals1782@naver.com>
2014-10-27 02:58:44 +0900

    hehehe

---
+++ gwajae2.py
@@ -0,0 +1,14 @@
+  1 # coding utf-8
+  2 
+  3 print "Hello, world!"
+  4 print 'hello', 'world!'
+  5 print 3
+  6 print 3.0
+  7 print 2 + 3
+  8 print "2" + "3"
+  9 print "2 + 3 =", 2+3
+ 10 print 2 * 3
+ 11 print 2 ** 3
+ 12 print 2 / 3
+ 13 print 2.0 / 3.0
+                                   
