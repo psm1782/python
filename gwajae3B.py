01ce6f3b5991e52c6ac4565313c976ae616c4038
psm1782 <tjdals1782@naver.com>
2014-11-11 19:02:57 +0900

    tuple

---
+++ gwajae3B.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+l1 = tuple(raw_input('Enter the name, age, score:').split(','))
+l2 = tuple(raw_input('Enter the name, age, score:').split(','))
+l3 = tuple(raw_input('Enter the name, age, score:').split(','))
+l4 = tuple(raw_input('Enter the name, age, score:').split(','))
+l5 = tuple(raw_input('Enter the name, age, score:').split(','))
+ 
+ 
+LisT = [l1, l2, l3, l4, l5]
+ 
+LisT.sort()
+ 
+print LisT
